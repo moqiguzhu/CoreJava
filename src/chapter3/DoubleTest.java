13623cb24ee3b8d40b358cd4f22fdae9c6b08fdc
mike <mikewell@163.com>
2015-03-24 08:55:01 +0800

    First Commit

---
+++ src/chapter3/DoubleTest.java
@@ -0,0 +1,7 @@
+package chapter3;
+
+public class DoubleTest {
+	public static void main(String[] args) {
+		System.out.println(2.0-1.1);
+	}
+}
---
+++ src/chapter3/Test.java
@@ -0,0 +1,10 @@
+package chapter3;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
